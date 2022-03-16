0f8063538396f90a693ea7ce5c277f9d1dfb0bca
Esther Anierobi <esther.anierobi@gmail.com>
2022-03-16 11:12:17 +0100

    Add files via upload

---
+++ photo gallery/script.js
@@ -0,0 +1,83 @@
+const change = document.getElementById("grape");
+
+change.addEventListener("click" , (event) => {
+    event.target.style.background = "darkblue";
+    event.target.style.color = "skyblue";
+});
+
+const change1 = document.getElementById("papaya");
+
+change1.addEventListener("click" , (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("apple");
+
+change2.addEventListener("click" , (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("strawberry");
+
+change3.addEventListener("click" , (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("orange");
+
+change4.addEventListener("click" , (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+});
+
+const change5 = document.getElementById("pineapple");
+
+change5.addEventListener("click" , (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("mangoe");
+
+change6.addEventListener("click" , (event) => {
+    event.target.style.background = "pink";
+    event.target.style.color = "black";
+});
+
+const change7 = document.getElementById("pear");
+
+change7.addEventListener("click" , (event) => {
+    event.target.style.background = "white";
+    event.target.style.color = "green";
+});
+
+const change8 = document.getElementById("watermelon");
+
+change8.addEventListener("click" , (event) => {
+    event.target.style.background = "grey";
+    event.target.style.color = "black";
+});
+
+const change9 = document.getElementById("kiwi");
+
+change9.addEventListener("click" , (event) => {
+    event.target.style.background = "violet";
+    event.target.style.color = "black";
+});
+
+const change10 = document.getElementById("banana");
+
+change10.addEventListener("click" , (event) => {
+    event.target.style.background = "indigo";
+    event.target.style.color = "white";
+});
+
+const change11 = document.getElementById("avocado");
+
+change11.addEventListener("click" , (event) => {
+    event.target.style.background = "seagreen";
+    event.target.style.color = "black";
+});
